c4017d9ff5ec8092bc292fe0813beb8e48c7bd68
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-27 19:09:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,14 +13,14 @@ class Artist
   end
 
   def add_song(song)
-    self.songs << song
+    @songs << song
     song.artist = self
     @@song_count +=1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    self.songs << song
+    @songs << song
     song.artist = self
     @@song_count +=1
   end
