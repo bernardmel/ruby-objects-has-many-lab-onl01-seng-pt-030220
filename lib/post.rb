1f630d1f89124d901fac558f6021b5d9ae8c959c
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-27 22:38:32 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,19 +1,22 @@
 class Post
 
-  attr_accessor :title, :author
+  attr_accessor :title :author
 
     @@all = []
 
   def initialize(new)
     @new = new
     @@all << self
-    @title = title
+    @title
   end
 
   def self.all
     @@all
   end
 
+  def self.title
+    title
+  end
 
   def author_name
       if @author == nil
